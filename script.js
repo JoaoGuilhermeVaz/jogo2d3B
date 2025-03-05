b981506809868806753630759d5142d6bba37ce1
JOÃO GUILHERME VAZ <joao_g_vaz@estudante.sesisenai.org.br>
2025-03-05 09:19:36 -0300

    inicializando js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-a=1
+//pegar o elemento canvas pelo id
+const canvas = document.getElementById('jogo2D')
+
+//inicializar o canvas
+const ctx = canvas.getcontext('2d')
+
+// criar a função loop
+function loop () {
+
+}
+loop()
