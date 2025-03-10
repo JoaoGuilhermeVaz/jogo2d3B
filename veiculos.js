eeebb9155d076e6b2a3268d42f41f942c9cdc1c7
JOÃO GUILHERME VAZ <joao_g_vaz@estudante.sesisenai.org.br>
2025-03-10 11:20:42 -0300

    veiculos

--- veiculos.js
+++ veiculos.js
@@ -1,26 +1,34 @@
-class Veículo {
-    construtor(tipo, marca, cor, velocidade, pasegeiros){
+class Veiculo {
+    constructor( tipo, marca,cor, velocidade, passageiros){
         this.tipo = tipo;
         this.marca = marca;
         this.cor = cor;
         this.velocidade = velocidade;
         this.passageiros = passageiros;
     }
-    acelerar = function () {
+    acelerar = function(){
         this.velocidade += 10
         console.log(this.velocidade)
     }
+    freiar = function(){
+        if (this.velocidade > 0){
+            this.velocidade -= 5
+            console.log(this.velocidade)
+        }else{
+            console.log("o carro já está parado")
+        }
+    }
 }
-freiar = function (){
-    if (this.velocidade > 0)
-        this.velocidade -= 5
-} else {
-    console.log('o carro ja esta parado')
-}
-
-const carro = new veiculo( 'Gol','Volkswagen','vinho', 0, 0)
-console.log(carro)
+const carro = new Veiculo(
+    'SUV',
+    'renaut',
+    'cinza',
+    0,
+    0
+)
+const outro_carro = new Veiculo('sedan','fiat', 'preto', 0,0)
+outro_carro.acelerar()
 carro.acelerar()
 carro.acelerar()
 carro.freiar()
-carro.acelerar()
+carro.freiar()
