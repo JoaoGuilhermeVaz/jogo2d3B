344dc4ec0524bff0dc1476725efa7b9425b02bfb
JOÃO GUILHERME VAZ <joao_g_vaz@estudante.sesisenai.org.br>
2025-03-12 11:14:57 -0300

    2 meses sem o gol

--- veiculos.js
+++ veiculos.js
@@ -19,16 +19,36 @@ class Veiculo {
         }
     }
 }
-const carro = new Veiculo(
-    'SUV',
-    'renaut',
-    'cinza',
-    0,
-    0
-)
+class Aviao extends Veiculo{
+    constructor(tipo, marca, cor, mach, passageiros, companhia){
+        super(tipo, marca, cor, mach, passageiros, companhia);
+        this.companhia = companhia;
+    }
+}
+
+class Barco extends Veiculo{
+    constructor(tipo, marca, cor, noz, tripulação, marina){
+    super(tipo, marca, cor, noz, tripulação, marina);
+    this.marina = marina;
+    }
+}
+
+
+const carro = new Veiculo('SUV','renaut','cinza',0,0)
 const outro_carro = new Veiculo('sedan','fiat', 'preto', 0,0)
 outro_carro.acelerar()
 carro.acelerar()
 carro.acelerar()
 carro.freiar()
 carro.freiar()
+console.log(carro)
+
+const aviao = new Aviao('Comercial', 'boeing', 'branco', 0, 0, 'azul')
+aviao.acelerar()
+aviao.freiar()
+console.log(aviao)
+
+const barco = new Barco('iate', 'seila', 'azul', 0,0, 'Biguaçu')
+barco.acelerar()
+barco.freiar()
+console.log(barco)
